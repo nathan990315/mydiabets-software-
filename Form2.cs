1b804b0127e21e88bb47f10d9a5c8076ac5cddf7
nathangiovannini3 <nathan95@live.it>
2017-01-21 13:48:51 +0100

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mydiabets_software
+{
+    public partial class Form2 : Form
+    {
+        internal static readonly object show;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
